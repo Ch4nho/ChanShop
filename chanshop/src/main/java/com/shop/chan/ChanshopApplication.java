3957eba0a9cc40b43449766a3c492b89e997dbea
CH KIM <98895025+Ch4nho@users.noreply.github.com>
2022-11-08 17:28:30 +0900

    #0 기초 파일 생성

---
+++ chanshop/src/main/java/com/shop/chan/ChanshopApplication.java
@@ -0,0 +1,13 @@
+package com.shop.chan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChanshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChanshopApplication.class, args);
+	}
+
+}
